d98fdc75e286a3c0742e33e4f2e745947f4e0974
brianqeep <brites009@gmail.com>
2017-02-08 12:56:06 +0300

    second test pass

--- lib/find_replace.rb
+++ lib/find_replace.rb
@@ -1,17 +1,13 @@
 class String
   define_method(:find_replace) do
-    # if self.eql?("cat")
-    #   self.gsub(self,"dog")
-    # end
+    
     letters = ["cat","cat","me"]
-    letters.split(",")
+
     for letter in letters
       if letters.include?("cat")
         letter.gsub!(letter,"dog")
-        letters.join(" ")
-
       end
-
     end
+    letters.join(" ")
   end
 end
--- spec/find_replace_spec.rb
+++ spec/find_replace_spec.rb
@@ -2,10 +2,7 @@ require('rspec')
 require('find_replace')
 
 describe('String#find_replace') do
-  # it("gets cat and replaces it with dog") do
-  #   expect(("cat").find_replace()).to(eq("dog"))
-  # end
   it("gets cat and replaces it with dog") do
-    expect(("cat cat me").find_replace()).to(eq(["dog dog me"]))
+    expect(("cat cat me").find_replace()).to(eq("dog dog me"))
   end
 end
